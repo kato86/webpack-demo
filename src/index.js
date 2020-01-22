2167cace23da31d71d4c9ac56cad8db93e846289
Kamil <tomala.kamil@gmail.com>
2020-01-22 15:12:39 +0100

    Load image

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import _ from "lodash";
-
 import "./style.css";
+import Photo from "./markus-spiske-qjnAnF0jIGk-unsplash.jpg";
 
 function component() {
   const element = document.createElement("div");
+  const myPhoto = new Image();
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
   element.classList.add("hello");
+
+  myPhoto.src = Photo;
+
+  element.appendChild(myPhoto);
 
   return element;
 }
