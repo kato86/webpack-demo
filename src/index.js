0d669412c4976bf4608680a0046bdfee607215ea
Kamil <tomala.kamil@gmail.com>
2020-01-22 15:39:43 +0100

    Cleanup

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import _ from "lodash";
-import "./style.css";
-import Photo from "./markus-spiske-qjnAnF0jIGk-unsplash.jpg";
-import Data from "./data.xml";
 
 function component() {
   const element = document.createElement("div");
   const myPhoto = new Image();
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-
-  myPhoto.src = Photo;
-
-  element.appendChild(myPhoto);
-
-  console.log(Data);
 
   return element;
 }
