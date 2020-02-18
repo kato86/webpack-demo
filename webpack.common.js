0cf7dfb8213d0faf12d52e5fb43447d652888310
Kamil <tomala.kamil@gmail.com>
2020-02-18 16:04:33 +0100

    Change webpack.common

--- webpack.common.js
+++ webpack.common.js
@@ -1,36 +1,19 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
     app: "./src/index.js"
   },
-  devtool: "inline-source-map",
-  devServer: {
-    contentBase: "./dist",
-    hot: true
-  },
-  mode: "development",
-  optimization: {
-    usedExports: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "Hot Module Replacement"
+      title: "Production"
     })
   ],
   output: {
-    filename: "boundle.js",
+    filename: "[name].boundle.js",
     path: path.resolve(__dirname, "dist")
   }
 };
