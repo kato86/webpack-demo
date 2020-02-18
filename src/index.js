7d73bf5dd649f8467200fd525a98b0914980be5f
Kamil <tomala.kamil@gmail.com>
2020-02-18 16:02:37 +0100

    Delete commented text

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import { cube } from "./math";
 
-// import _ from "lodash";
-// import printMe from "./print";
-
-// import "./styles.css";
-
 function component() {
-  // const element = document.createElement("div");
   const element = document.createElement("pre");
   const btn = document.createElement("button");
 
@@ -14,26 +8,7 @@ function component() {
     "\n\n"
   );
 
-  // element.innerHTML = _.join(["Hello", "webpack"], " ");
-
-  // btn.innerHTML = "Click me and check the console!";
-  // btn.onclick = printMe;
-
-  // element.appendChild(btn);
-
   return element;
 }
 
 document.body.appendChild(component);
-
-// let element = component();
-// document.body.appendChild(element);
-
-// if (module.hot) {
-//   module.hot.accept("./print.js", function() {
-//     console.log("Accepting the updated printMe module!");
-//     document.body.removeChild(element);
-//     element = component();
-//     document.body.appendChild(element);
-//   });
-// }
