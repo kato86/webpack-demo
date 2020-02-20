0d1afe2bb23f01cd0a3da4d0469748b7979eb2b1
Kamil <tomala.kamil@gmail.com>
2020-02-20 10:48:10 +0100

    Import lodash and add button to print text

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { cube } from "./math";
+import _ from "lodash";
 
 if (process.env.NODE_ENV !== "production") {
   console.log("Looks like we are in development mode!");
@@ -6,13 +7,28 @@ if (process.env.NODE_ENV !== "production") {
 
 function component() {
   const element = document.createElement("pre");
+  const divElement = document.createElement("div");
   const btn = document.createElement("button");
+  const br = document.createElement("br");
+  const hr = document.createElement("hr");
 
   element.innerHTML = ["Hello webpack!", "5 cubed is equal to " + cube(5)].join(
     "\n\n"
   );
+  btn.innerHTML = "Click me and look at the console!";
+  divElement.innerHTML = _.join(["Hello", "webpack"], " ");
+  divElement.appendChild(br);
+  divElement.appendChild(hr);
+  divElement.appendChild(btn);
 
-  return element;
+  btn.onclick = e =>
+    import(/* webpackChunkName: "print" */ "./print").then(module => {
+      const print = module.default;
+
+      print();
+    });
+
+  return divElement;
 }
 
-document.body.appendChild(component);
+document.body.appendChild(component());
